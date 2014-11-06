c9402fffdaa7e6780c03bd634b7299968eefdea4
MrWizard666 <kotzelindsay@gmail.com>
2014-11-06 12:51:53 +0200

    Initial website skeleton commit

---
+++ MultiPolls/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MultiPolls.MvcApplication" Language="C#" %>
---
+++ MultiPolls/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MultiPolls.Startup))]
+namespace MultiPolls
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
