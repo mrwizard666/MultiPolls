b3be87189e89c67efdc0359e3a729ec29f20cad5
MrWizard666 <kotzelindsay@gmail.com>
2014-11-06 14:25:42 +0200

    Remove Home from MapRoute

--- MultiPolls/App_Start/RouteConfig.cs
+++ MultiPolls/App_Start/RouteConfig.cs
@@ -14,6 +14,12 @@ namespace MultiPolls
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "OnlyAction",
+                url: "{action}",
+                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+             );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
