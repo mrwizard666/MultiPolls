84dafa9df06834c1754da6ca156958f0c3a61c95
MrWizard666 <kotzelindsay@gmail.com>
2014-11-07 12:47:35 +0200

    Insert login home page with placeholder images

--- MultiPolls/Controllers/HomeController.cs
+++ MultiPolls/Controllers/HomeController.cs
@@ -12,21 +12,9 @@ namespace MultiPolls.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult LoginHome()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
     }
-
 }
