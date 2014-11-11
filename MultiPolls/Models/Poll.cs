bdfd9d5bb2e385c152c91f7a90c5e7d2209b6008
MrWizard666 <kotzelindsay@gmail.com>
2014-11-11 11:41:31 +0200

    Create polls controller. Write jquery script for create poll page.

--- MultiPolls/Models/Poll.cs
+++ MultiPolls/Models/Poll.cs
@@ -10,16 +10,26 @@ namespace MultiPolls.Models
         public DateTime EditDate { get; set; }
         public string CreatorID { get; set; }
         public string PollQuestion { get; set; }
-        public int OptionA { get; set; }
-        public int OptionB { get; set; }
-        public int OptionC { get; set; }
-        public int OptionD { get; set; }
-        public int OptionE { get; set; }
-        public int OptionF { get; set; }
-        public int OptionG { get; set; }
-        public int OptionH { get; set; }
-        public int OptionI { get; set; }
-        public int OptionJ { get; set; }
+        public string OptionA { get; set; }
+        public string OptionB { get; set; }
+        public string OptionC { get; set; }
+        public string OptionD { get; set; }
+        public string OptionE { get; set; }
+        public string OptionF { get; set; }
+        public string OptionG { get; set; }
+        public string OptionH { get; set; }
+        public string OptionI { get; set; }
+        public string OptionJ { get; set; }
+        public int AnswerA { get; set; }
+        public int AnswerB { get; set; }
+        public int AnswerC { get; set; }
+        public int AnswerD { get; set; }
+        public int AnswerE { get; set; }
+        public int AnswerF { get; set; }
+        public int AnswerG { get; set; }
+        public int AnswerH { get; set; }
+        public int AnswerI { get; set; }
+        public int AnswerJ { get; set; }
     }
 
     public class PollDBContext : ApplicationDbContext
---
+++ MultiPolls/Scripts/create.poll.jquery.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+    $(".make-invisible").hide();
+});
+
+$(document).ready(function () {
+    var x = 2;
+    var y = 3;
+ 
+    $("[id^=option]").keyup(function () {
+        if ("option" + x === this.id && x !== 10) {
+            $("#Option" + y).show();
+            x++;
+            y++
+        };
+    });
+});
